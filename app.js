39404e250f95c7f3417937dc976bc51a31dd62cc
Dimitrios Kalaitzidis <kalaitzidis34@gmail.com>
2019-09-06 13:11:49 +0300

    First CRUD endpoint

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('Home')
+});
+
+
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+{ useNewUrlParser: true },
+    () => console.log('connected to DB')
+);
+
+
+//Start Server 
+app.listen(3000);
